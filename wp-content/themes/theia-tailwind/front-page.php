d0b68924ed9555879e816436431503fdc3e6be54
Kevin Lim <zzkevinlim@gmail.com>
2021-05-11 12:02:45 +1200

    202105112021

--- wp-content/themes/theia-tailwind/front-page.php
+++ wp-content/themes/theia-tailwind/front-page.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+<!-- load more data on click - https://codepen.io/Nirvana11/pen/MZbxza -->
 <!-- import/export posts to excel - https://artisansweb.net/export-posts-csv-wordpress/ https://www.sitepoint.com/wordpress-headless-cms-eleventy/ -->
 <!-- vue apollo graphql woocommerce - https://apollo.vuejs.org/ -->
 <?php get_template_part( 'template-parts/content', 'header-menu' ); ?>
