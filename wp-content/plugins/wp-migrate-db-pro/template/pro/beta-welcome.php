1d7b372ab911b7c5c485662b89d0c3d8e223053a
Kevin Lim <zzkevinlim@gmail.com>
2021-06-25 16:29:11 +1200

    202106251629

---
+++ wp-content/plugins/wp-migrate-db-pro/template/pro/beta-welcome.php
@@ -0,0 +1,4 @@
+<div class="below-title beta-welcome warning inline-message">
+	<h3>THANKS FOR TRYING THE BETA OF WP MIGRATE DB PRO 2.0</h3>
+	<p>Please report any bugs you find on the <a href="#help">Help tab</a>.</p>
+</div>
